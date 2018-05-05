9fb44bb2d2613a934ed280c6ba485966383c6cdc
tianyao1916 <tianyao1916@163.com>
2018-05-05 18:08:29 +0800

    update by tianyao

--- src/main/java/com/tianyao/controller/GuideInfoController.java
+++ src/main/java/com/tianyao/controller/GuideInfoController.java
@@ -3,9 +3,11 @@ package com.tianyao.controller;
 import com.tianyao.entity.GuideInfoEntity;
 import com.tianyao.service.GuideInfoService;
 import com.tianyao.util.MyPropertiesUtil;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.impl.XMLResponseParser;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,15 +24,37 @@ public class GuideInfoController {
     private GuideInfoService guideInfoService;
 
     @RequestMapping("/serchGuideInfo")
+    @ResponseBody
     public String serchGuideInfo(String keywords) {
+        // 获得solr的客户端连接
+        HttpSolrServer solr = new HttpSolrServer(MyPropertiesUtil.getMyProperty("mySolr.properties", "solr_url"));
+        solr.setParser(new XMLResponseParser());
+        solr.setConnectionTimeout(3000);
 
-        return keywords;
+        // 定义查询参数
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery("operator:徐");
+        solrQuery.setRows(50);
+        solrQuery.setHighlight(true);
+        // 向solr查询数据
+        QueryResponse query = null;
+        try {
+            query = solr.query(solrQuery);
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        }
+
+        List<GuideInfoEntity> beans = query.getBeans(GuideInfoEntity.class);
+
+        System.out.println(beans);
+        return beans.toString();
     }
 
     @RequestMapping(value = "importData"/*,produces = "application/json;charset=utf-8"*/, method = RequestMethod.GET)
     @ResponseBody
     public String importData() {
         List<GuideInfoEntity> guideInfoEntities = guideInfoService.queryAllGuideInfo();
+        System.out.println(guideInfoEntities);
         // 获得solr的客户端连接
         HttpSolrServer solr = new HttpSolrServer(MyPropertiesUtil.getMyProperty("mySolr.properties", "solr_url"));
         solr.setParser(new XMLResponseParser());
@@ -44,10 +68,12 @@ public class GuideInfoController {
             solr.commit();
         } catch (Exception e) {
             e.printStackTrace();
-            return "导入失败！！！";
+            return e.toString();
+            //return e.getCause().toString();
+           // return "导入失败！！！";
         }
 
-        System.out.println(guideInfoEntities);
+
         return "导入成功！！！";
     }
 
